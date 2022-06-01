4b74150d8ea97cdd0a397359d80ac3184ab8ce8b
Rahul <rhl.goyal.123@gmail.com>
2022-06-01 21:01:36 +0530

    intial changes

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ for(item of btn){
 item.addEventListener('click', (e)=>{
     btnText = e.target.innerText;
     if(btnText == '*'){
-        srn.value += btnText;
+        srn.innerText += btnText;
     }
  else if(btnText == "="){
-        srn.value = eval(srn.value);
+        srn.innerText = eval(srn.innerText);
     }
     else if ( btnText == 'C'){
-        srn.value = '';
+        srn.innerText = '';
     }
     else if( btnText == '%'){
-        srn.value = srn.value*1/100;
+        srn.innerText = srn.innerText*1/100;
     }
     else if(btnText =='<'){
-        let string = srn.value.toString();
-        srn.value = string.substr(0, string.length - 1);
+        let string = srn.innerText.toString();
+        srn.innerText = string.substr(0, string.length - 1);
     }
     else{
-        srn.value += btnText;
+        srn.innerText += btnText;
     }
 })
 };
